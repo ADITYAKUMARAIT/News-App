48cc3843e9954ef3cb4916a173dfa45a86da8a5e
Aditya_kumar <adityakumar9815712240@gmail.com>
2023-09-22 23:34:49 +0530

    conversion of textle into newsapp

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
-// import logo from './logo.svg';
-import Navbar from './components/Navbar'
-import './App.css';
-import TextForm from './components/TextForm';
-import { useState } from 'react';
-import Alert from './components/Alert';
-// import About from './components/About'
+import React, { Component } from 'react'
 
-function App() {
-  const [mode,setMode] = useState('light');
-  const [alert ,setalert] = useState(null);
-  const showalert =(message,type)=>{
-    setalert({
-      message:message,
-      type:type
-    })
-    setTimeout(()=>{
-      setalert(null);
-    },1000);
-
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
   }
-  const toggleMode =()=>{
-    if(mode==='dark'){
-      setMode('light')
-      document.body.style.backgroundColor = 'white';
-      showalert("light mode is enabled","success");
-    }
-    else{
-      setMode('dark');
-      document.body.style.backgroundColor = 'grey'
-      showalert("dark mode is enabled","success");
-    }
-  }
-  return (
-    <>
-      <Navbar title="Textutils" about="About"  mode={mode}  toggleMode ={toggleMode}/>
-      <Alert  alert ={alert}/>
-      <div className="container"><TextForm heading="Enter the text to analyze below" mode={mode} showalert={showalert} /></div>
-      {/* <About/> */}
-    </>
-  );
 }
 
-export default App; 
