ae8aec6575f56bddec9a4400a25ee822622e1488
Aditya_kumar <adityakumar9815712240@gmail.com>
2023-09-23 18:57:24 +0530

    creation of the component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import Navbar from './component/Navbar'
+import News from './component/News'
 
 export default class App extends Component {
   render() {
     return (
+      <>
       <div>
+        <Navbar/>
+        <News/>
         
       </div>
+      </>
     )
   }
 }
---
+++ src/component/NewsItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        this is the section for the news headline
+        
+      </div>
+    )
+  }
+}
+
+export default NewsItem
